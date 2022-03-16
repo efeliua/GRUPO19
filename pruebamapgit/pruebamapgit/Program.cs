956b45805e78bdb38a931f3dd6530d4df93b7683
Eva Feliu Arejola <efeliu@ucm.es>
2022-03-16 09:54:14 +0100

    Archivos inciales añadidos al índice del repositorio

---
+++ pruebamapgit/pruebamapgit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pruebamapgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi!");
+        }
+    }
+}
